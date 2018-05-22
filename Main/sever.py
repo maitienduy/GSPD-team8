fa03e12b6830917f293d4788fd1708710541163c
maitienduy <maitienduy@gmail>
2018-05-22 09:50:28 +0700

    add UV

--- Main/sever.py
+++ Main/sever.py
@@ -5,8 +5,8 @@ print("Start")
 port="COM4"#This will be different for various devices and on windows it will probably be a COM port.
 bluetooth=serial.Serial(port, 9600, timeout=1)#Start communications with the bluetooth unit
 print("Connected")
-#bluetooth.flushInput() #This gives the bluetooth a little kick
-bluetooth.write(b"data test")
+bluetooth.flushInput() #This gives the bluetooth a little kick
+#bluetooth.write(b"data test")
 while True :
         input_data=bluetooth.readline()#This reads the incoming data. In this particular example it will be the "Hello from Blue" line
         print(input_data.decode())
--- Main/uno-server/uno-server.ino
+++ Main/uno-server/uno-server.ino
@@ -1,34 +1,42 @@
 #include "DHT.h"
 #include <ctype.h>
 #include <SoftwareSerial.h>
+#include <Wire.h>
+#include "Adafruit_SI1145.h"
 
-#define DHTPIN 7     // what digital pin we're connected to
-#define DHTTYPE DHT22   
-#define BUFFER_SIZE 64//This will prevent buffer overruns.
+Adafruit_SI1145 uv = Adafruit_SI1145(); //connect the UV sensor SCL to A5, SDA to A4
+#define DHTPIN 7     // what digital pin we're connected DHT22 to
+#define DHTTYPE DHT22   //define the type of DHT sensor
+#define BUFFER_SIZE 64
 DHT dht(DHTPIN, DHTTYPE);
 /*Buffer setup*/
-char inData[BUFFER_SIZE];//This is a character buffer where the data sent by the python script will go.
-//char inChar=-1;//Initialie the first character as nothing
-/*blutooth setup*/
-SoftwareSerial serial_connection(10, 11);//Create a serial connection with TX and RX on these pins
+char inData[BUFFER_SIZE];
+/*bluetooth setup*/
+SoftwareSerial serial_connection(10, 11);//From bluetoothmodule, connect TX to 10 and RX to 11 in arduino
 
 void setup() {
   Serial.begin(9600);
   serial_connection.begin(9600);//Initialize communications with the bluetooth module
-  serial_connection.println("Ready!!!");//Send something to just start comms. This will never be seen.
+  serial_connection.println("Ready!!!");//Send something to just starting the  comms. Can not be seen.
   Serial.println("Started");//Tell the serial monitor that the sketch has started.
   dht.begin();
+  if (! uv.begin()) {
+    Serial.println("Didn't find Si1145");
+    while (1);
+  }
 }
 
 void loop() {
   // delay for preparing the sensor
- delay(2000);
+ delay(5000);
  /*sending*/
  serial_connection.print("{ humiditi: ");/*sending humidity data*/
  serial_connection.println(hum());
- serial_connection.print("temprature: ");
+ serial_connection.print("temprature: ");/*sending temp data*/
  serial_connection.println(temp());
- serial_connection.println("}");/*sending temp data*/
+  serial_connection.print("UVindex: ");/*sending UV data*/
+ serial_connection.println(getuv());
+ serial_connection.println("}");
  /*receiving*/
  int i=0;
  byte byte_count=serial_connection.available();//This gets the number of bytes that were sent by the python script
@@ -55,5 +63,15 @@ float temp(){
     }
   return t;
 }
-
+/* read UV sensor */
+float getuv(){
+ // Serial.print("Vis: "); Serial.println(uv.readVisible());
+ // Serial.print("IR: "); Serial.println(uv.readIR()); 
+  float UVindex = uv.readUV();
+  // the index is multiplied by 100 so to get the
+  // integer index, divide by 100!
+  UVindex /= 100.0;  
+  Serial.print("UV: ");  Serial.println(UVindex);
+  return UVindex;
+}
 
